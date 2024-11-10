4fe4dae80d42a76f436a317a212da679e079c562
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-10 19:12:57 +0100

    Minor bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,18 @@ const Navbar = () =>
     return (
         <nav className="bg-teal-800 flex justify-between items-center h-[70px] px-8 border-b-[3px] border-orange-500">
             <Link href='/'>
-                <h1 className="text-xl logo text-white">
-                    Acme <span className="text-orange-500">Website</span>
+                <h1 className="logo text-white text-2xl">
+                    Acme <span className="text-orange-500">Web Design</span>
                 </h1>
             </Link>
             <ul className="flex gap-10">
-                <li className="text-lg">
+                <li className="text-xl">
                     <Link href={'/'} className={currentPath === '/' ? 'text-orange-500' : 'text-white duration-150 ease-in hover:text-orange-500'}>Home</Link>
                 </li>
-                <li className="text-lg">
+                <li className="text-xl">
                     <Link href={'/about'} className={currentPath === '/about' ? 'text-orange-500' : 'text-white duration-150 ease-in hover:text-orange-500'}>About</Link>
                 </li>
-                <li className="text-lg">
+                <li className="text-xl">
                     <Link href={'/offers'} className={currentPath === '/offers' ? 'text-orange-500' : 'text-white duration-150 ease-in hover:text-orange-500'}>Offers</Link>
                 </li>
            </ul>
